543ed4b9965c3be2a54497d1f3c80c2525e034eb
Nirusu <nirusu@users.noreply.github.com>
2017-07-16 22:59:19 +0200

    Krypto 2 - added Square and Multiply -> LaTeX tool

---
+++ Einführung in die Kryptographie/SquareAndMultiply/SquareAndMultiply/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SquareAndMultiply
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            const ulong zahl = 23;
+            const ulong exponent = 37;
+            const ulong modulo = 127;
+            ulong currentNumber = zahl;
+            var zahlBinary = Convert.ToString((uint) exponent, 2);
+            Console.Write("SQM: \\\\" + Environment.NewLine + exponent + " = " + zahlBinary + " \\\\" + Environment.NewLine + "\\textbf{1} \\quad " + zahl + " \\\\" + Environment.NewLine);
+            for (var i = 1; i < zahlBinary.Length; i++)
+            {
+                if (zahlBinary[i].Equals('1'))
+                {
+                    Console.Write("\\textbf{1} \\quad " + zahl + '*' + currentNumber + "^2\\mod " + modulo + " = ");
+                    currentNumber = (zahl * (ulong) Math.Pow(currentNumber, 2)) % modulo;
+                    Console.Write(currentNumber + " \\\\" + Environment.NewLine);
+                }
+                else
+                {
+                    Console.Write("\\textbf{0} \\quad " + currentNumber + "^2\\mod " + modulo + " = ");
+                    currentNumber = (ulong) Math.Pow(currentNumber, 2) % modulo;
+                    Console.Write(currentNumber + " \\\\" + Environment.NewLine);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
